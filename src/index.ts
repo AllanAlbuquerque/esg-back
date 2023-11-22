3ce40052a97bde8a428973f2f824fdf94afec44d
Allan Albuquerque <allan@xmiiconsulting.com>
2023-11-21 21:11:04 -0300

    Add cors package and update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import 'reflect-metadata';
-import * as express from 'express';
+import express from 'express';
 import * as bodyParser from 'body-parser';
 import { AppDataSource } from './data-source';
 import routes from './routes';
+import cors from 'cors';
 
 AppDataSource.initialize().then(async () => {
   const app = express();
+
+  app.use(cors());
   app.use(bodyParser.json());
   app.use(routes);
 
